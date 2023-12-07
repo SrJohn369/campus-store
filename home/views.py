1948c87c0b7e447250b08f22715f1c7045b790ce
João Victor <106630200+SrJohn369@users.noreply.github.com>
2023-12-07 17:40:29 -0300

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from cadastro.models import Produto
+from cadastro.models import Produto, Vendedor
 
 
 # Create your views here.
@@ -7,3 +7,17 @@ def home(request):
     data_produto = Produto.objects.all()
     
     if request.method == 'GET': return render(request, "home.html", {'data_protuto': data_produto})
+
+
+def produtos(request):
+    data_protuto_all = Produto.objects.all()
+
+    if request.method == 'GET': return render(request, "produtos.html", {'data_protuto_all': data_protuto_all})
+
+
+def vendedores(request):
+    data_vendedor_all = Vendedor.objects.all()
+
+    if request.method == 'GET': return render(request, "vendedores.html", {'data_vendedor_all': data_vendedor_all})
+
+    
